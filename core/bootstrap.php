102657246611a1dd67f49028bb3ddfb124948238
flaver <flaverkleiber@yahoo.de>
2013-12-28 19:09:59 +0100

    finshed the init

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,7 +1,13 @@
 <?php
+/**
+ * Bootstrap
+ *
+ * @author Flavio Kleiber flavio@swagpeople.ch
+ * @copyright Informatik Atelie GmbH (c) 2013
+ */
 $config = parse_ini_file('./config/bf_MainConfig.ini');
 define('BASE_DIR', $config['site']['BASE_DIR']);
 define('HTTP_PATH', 'http://'. $_SERVER['SERVER_NAME'] . '/' .BASE_DIR);
 define('FS_PATH', $_SERVER['DOCUMENT_ROOT']. '/' . BASE_DIR);
-define('REQUEST_URI', preg_match('/\?', $_SERVER['QUERY_STRING'] . '/i', '', $_SERVER['REQUEST_URI']));
+define('REQUEST_URI', preg_replace('/\?' . $_SERVER['QUERY_STRING'] . '/i', '', $_SERVER['REQUEST_URI']));
 require_once(ROOT. '/core/init.php');
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
+/**
+ * Index
+ *
+ * @author Flavio Kleiber flavio@swagpeople.ch
+ * @copyright Informatik Atelie GmbH (c) 2013
+ */
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/core/bootstrap.php');
