231648c2e720fb805db9dc41d5a047ef4af35532
flaver <flaverkleiber@yahoo.de>
2013-12-30 18:52:39 +0100

    reset

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Index
- *
- * @author Flavio Kleiber flavio@swagpeople.ch
- * @copyright Informatik Atelie GmbH (c) 2013
- */
-define('ROOT', dirname(__FILE__));
-require_once(ROOT.'/core/bootstrap.php');
