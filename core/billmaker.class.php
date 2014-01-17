528f04b8a69169845ccebee1b60abbe084488eb2
oneaurel <aurel1303@hotmail.com>
2014-01-17 11:37:09 +0100

    Started whit bill BITCHES!

---
+++ core/billmaker.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author syz <aurel@swagpeople.ch>
+ * @copyright syz, swagpeople.ch (c) 2014
+ *
+ * This is the bill maker!
+ **/
+ class BillMaker {
+	private Bill;
+	
+	$this->Bill = 10;
+ 
+ }
