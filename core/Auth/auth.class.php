41f063ad14bd035fa63ea6fcd5f092d8dc92b744
oneaurel <aurel1303@hotmail.com>
2014-02-19 10:13:40 +0100

    Autologin try and Cookie hasching

--- core/Auth/auth.class.php
+++ core/Auth/auth.class.php
@@ -21,18 +21,45 @@ class Auth {
 
 	/*
 	 * Because of errors its in the comments now
-	 * public function autolog() {
+	 * 
+	 */
+	public function autolog($hash) {
 		// Someone smart have to work here
-		 if exist cookie
-		get cookie
-		 $this->Auth->Login(CookieVariableUser,CookieVariable)
 		
 		
+		$row = new DBRow();
+    $row->setRow('bUser');
+	//die Usernamerainbowtabelrow  muss noch hinzugefügt werden
+    $row->set($User, 'username');
+	    $row->set($hash, 'usernamerainbowtabel');
+
+    $result = $row->findByKey();
+	if (empty($result))
+	{
+	return false;
+	}
+	else {
+	/**is now fixed**/
+	$_SESSION['loggedIn'] = true;
 		
-	}*/
+		return $result;
+	}}
 	
 	public function Login($User,$Password)
 	{
+
+
+if(isset($_COOKIE['autologger']))
+	{
+	$hash = $_COOKIE['autologger']; 
+	$userhash = $this->Auth->autolog($hash);}
+	if($userhash != false){
+	$q = "INSERT INTO User (Usernamerainbowtabel) VALUES ('$userhash') WHERE username ='$User';";
+	$this->$DB->sendquery();
+	}
+	else {
+	
+ 
     //Make a md5 hash of the pw
     $Password = md5($Password);
     $row = new DBRow();
@@ -46,7 +73,9 @@ class Auth {
 	}
 	else {
 	/**is now fixed**/
+	$Month = 2592000 + time(); 
+	  setcookie(autologger,($cookiestring = md5($User)), $Month);
 	$_SESSION['loggedIn'] = true;
 	return $result;}
-	}
+	}}
 }
--- index.php
+++ index.php
@@ -6,6 +6,9 @@
  * This is the Indexfile,
  * do only changes here when you know what you are doing!
  */
+ 
+
+
 define('ROOT', dirname(__FILE__));
 $url = isset($_GET['url']) ? $_GET['url'] : NULL;
 
